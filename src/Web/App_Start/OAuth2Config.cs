13b8272bff6ea02cb7e0872aa0c549e27c24961f
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-02-09 22:51:12 +0100

    Refactored oauth2 config, minor cleanup.

--- src/Web/App_Start/OAuth2Config.cs
+++ src/Web/App_Start/OAuth2Config.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
 
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
@@ -10,6 +14,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Facebook;
 using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Owin;
 
@@ -19,40 +24,121 @@ namespace Web
     {
         public static readonly OAuthBearerAuthenticationOptions OAuthBearerOptions = new OAuthBearerAuthenticationOptions();
 
+        private static readonly double TokenExpire = TimeSpan.Parse(
+            ConfigurationManager.AppSettings.Get("Token.ExpireTimeSpan")).TotalSeconds;
+
         public static void Configuration(IAppBuilder app)
         {
-            var c = new CookieAuthenticationOptions 
-            {
-                 CookieHttpOnly = true,
-                 ExpireTimeSpan = TimeSpan.FromSeconds(30)
-            };
+            var c = ConfigureCookieAuthentication();
             app.UseCookieAuthentication(c);
             app.SetDefaultSignInAsAuthenticationType(c.AuthenticationType);
+            app.UseOAuthBearerAuthentication(ConfigureBearerAuthentication());            
             app.UseGoogleAuthentication(ConfigureGoogleAuthentication());
-            app.UseFacebookAuthentication(ConfigureFacebookAuthentication());
-            app.UseOAuthBearerAuthentication(OAuthBearerOptions);
+            app.UseFacebookAuthentication(ConfigureFacebookAuthentication());            
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);            
         }
+
+        #region Configure Cookie Authentication
+
+        private static CookieAuthenticationOptions ConfigureCookieAuthentication()
+        {
+            return new CookieAuthenticationOptions
+            {                
+                CookieHttpOnly = true,
+                ExpireTimeSpan = TimeSpan.FromSeconds(30)
+            };
+        }
+
+        #endregion
+
+        #region Configure Bearer Authentication
+
+        private static OAuthBearerAuthenticationOptions ConfigureBearerAuthentication()
+        {
+            OAuthBearerOptions.Provider = new OAuthBearerAuthenticationProvider
+            {
+                OnApplyChallenge = ctx =>
+                {
+                    var auth = ctx.OwinContext.Authentication;
+                    var claim = auth.User.FindFirst(ClaimTypes.Email);
+                    if (claim == null)
+                    {
+                        return Task.FromResult(false);
+                    }
+
+                    var options = OAuth2Config.OAuthBearerOptions;
+                    var token = options.AccessTokenFormat.Protect(new AuthenticationTicket(
+                        new ClaimsIdentity(new[] { claim }, options.AuthenticationType),
+                        new AuthenticationProperties() { ExpiresUtc = options.SystemClock.UtcNow.AddSeconds(OAuth2Config.TokenExpire) }));
+
+                    var props = auth.AuthenticationResponseChallenge.Properties;
+                    var returnUrl = props.RedirectUri;
+                    if (returnUrl != null)
+                    {
+                        var uri = new UriBuilder(returnUrl);
+                        var query = HttpUtility.ParseQueryString(uri.Query);
+                        query.Add("token", token);
+                        query.Add("expires", OAuth2Config.TokenExpire.ToString());
+                        uri.Query = query.ToString();
+
+                        auth.SignOut();
+                        ctx.Response.Redirect(uri.ToString());
+                        return Task.FromResult(true);
+                    }
+
+                    if (props.AllowRefresh.GetValueOrDefault(false))
+                    {
+                        ctx.Response.StatusCode = 201;
+                        ctx.Response.ContentType = "application/json";
+                        var content = string.Format("{{\"token\":\"{0}\",\"expires\":{1}}}", token, OAuth2Config.TokenExpire);
+                        ctx.Response.ContentLength = content.Length;
+                        ctx.Response.Write(content);
+                    }
+
+                    return Task.FromResult(true);
+                }
+            };
+            return OAuthBearerOptions;
+        }
+
+        #endregion
+
+        #region Configure Google Authentication
 
         private static GoogleOAuth2AuthenticationOptions ConfigureGoogleAuthentication()
         {
             var settings = ConfigurationManager.AppSettings;
             var options = new GoogleOAuth2AuthenticationOptions
             {
+                AuthenticationType = "google",
                 ClientId = settings.Get("Google.ClientId"),
                 ClientSecret = settings.Get("Google.ClientSecret"),
                 CallbackPath = new PathString("/oauth2/google/callback")
             };
             options.Scope.Clear();
             options.Scope.Add("email");
+            options.Provider = new GoogleOAuth2AuthenticationProvider
+            {
+                OnAuthenticated = ctx =>
+                {
+                    CleanUpAllClaimsButEmail(ctx.Identity);
+                    return Task.FromResult(true);
+                }
+            };
+
             return options;
         }
+
+        #endregion
+
+        #region Configure Facebook Authentication
 
         private static FacebookAuthenticationOptions ConfigureFacebookAuthentication()
         {
             var settings = ConfigurationManager.AppSettings;
             var options = new FacebookAuthenticationOptions
             {
+                AuthenticationType = "facebook",
                 AppId = settings.Get("Facebook.AppId"),
                 AppSecret = settings.Get("Facebook.AppSecret"),
                 CallbackPath = new PathString("/oauth2/facebook/callback")
@@ -63,27 +149,52 @@ namespace Web
             {
                 OnAuthenticated = async ctx =>
                 {
+                    CleanUpAllClaimsButEmail(ctx.Identity);
                     if (ctx.Email != null)
                     {
                         return;
                     }
 
-                    using (var client = new HttpClient())
+                    var email = await FindEmailInFacebook(options.UserInformationEndpoint, ctx.AccessToken);
+                    if (email == null)
                     {
-                        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ctx.AccessToken);
-                        var response = await client.GetAsync(options.UserInformationEndpoint + "?fields=email");
-                        if (!response.IsSuccessStatusCode)
-                        {
-                            return;
-                        }
-
-                        var d = JObject.Parse(await response.Content.ReadAsStringAsync());
-                        ctx.Identity.AddClaim(new Claim(ClaimTypes.Email, (string)d["email"]));
+                        return;
                     }
+
+                    ctx.Identity.AddClaim(new Claim(ClaimTypes.Email, email));
                 }
             };
             
             return options;
+        }
+
+        private static async Task<string> FindEmailInFacebook(string userInformationEndpoint, string accessToken)
+        {
+            string content;
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                var response = await client.GetAsync(userInformationEndpoint + "?fields=email");
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+
+                content = await response.Content.ReadAsStringAsync();
+            }
+
+            var d = JObject.Parse(content);
+            return (string)d["email"];
+        }
+
+        #endregion
+
+        private static void CleanUpAllClaimsButEmail(ClaimsIdentity identity)
+        {
+            foreach (var c in identity.Claims.Where(c => c.Type != ClaimTypes.Email).ToArray())
+            {
+                identity.RemoveClaim(c);
+            }
         }
     }
 }
