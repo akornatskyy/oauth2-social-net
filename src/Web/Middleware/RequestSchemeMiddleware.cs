b010af50fae390f74904006b3ec6ad7e2e37e50f
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2016-04-05 21:08:52 +0300

    Introduced request scheme middleware to handle X-Forwarded-Proto HTTP header.

---
+++ src/Web/Middleware/RequestSchemeMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Owin;
+
+namespace Web.Middleware
+{
+    public sealed class RequestSchemeMiddleware : OwinMiddleware
+    {
+        public RequestSchemeMiddleware(OwinMiddleware next)
+            : base(next)
+        {
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            if (string.Equals(context.Request.Headers["X-Forwarded-Proto"], "https", StringComparison.InvariantCultureIgnoreCase))
+            {
+                context.Request.Scheme = "https";
+            }
+
+            return Next.Invoke(context);
+        }
+    }
+}
--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -3,6 +3,8 @@
 using Microsoft.Owin;
 using Owin;
 
+using Web.Middleware;
+
 [assembly: OwinStartupAttribute(typeof(Web.Startup))]
 
 namespace Web
@@ -11,6 +13,7 @@ namespace Web
     {
         public void Configuration(IAppBuilder app)
         {
+            app.Use(typeof(RequestSchemeMiddleware));
             OAuth2Config.Configuration(app);
             var config = new HttpConfiguration();
             WebApiConfig.Register(config);
